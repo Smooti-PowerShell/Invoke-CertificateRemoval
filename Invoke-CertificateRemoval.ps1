3fc4aad492775e89803ea63c1cc9e16a082d9953
smooti <66232763+smooti@users.noreply.github.com>
2020-12-31 20:11:25 -0500

    Refactored Invoke-CertificateRemoval
    Removed list and foreach statement used for testingh. Added comments to script

--- Invoke-CertificateRemoval.ps1
+++ Invoke-CertificateRemoval.ps1
@@ -1,30 +1,39 @@
-$Path = "C:\Users\"
+<#
+    .SYNOPSIS
+        Removes all Software certificate installation files.
+    .NOTES
+        V-63393 - Software certificate installation files must be removed from a system.
 
-$List = (Get-ChildItem "$($Path)").FullName
-ForEach ($L in $List) {
+        Description - Use of software certificates and their accompanying installation files
+        for end users to access resources is less secure than the use of hardware-based certificates.
+    .PARAMETER Path
+        DEFAULT: C:\Users\
+        Path you would like to enumerate (This will be recursive).
+    .EXAMPLE
+        Invoke-CertificateRemoval         
+#>
+
+Param(
+    [string] $Path = 'C:\Users\'
+)
+
+$pfx = @(Get-ChildItem "$($Path)*.pfx" -Recurse).FullName
+$p12 = @(Get-ChildItem "$($Path)*.p12" -Recurse).FullName
+
+ForEach ($p in $pfx) {
     Try {
-        $pfx = @(Get-ChildItem "$($Path)*.pfx" -Recurse).FullName
-        $p12 = @(Get-ChildItem "$($Path)*.p12" -Recurse).FullName
+        Remove-Item $p -Force
     }
     Catch {
-        Write-Warning "Access to $($L) denied."
+        Write-Warning "Unable to remove '$($p)'"
     }
+}
 
-    ForEach ($p in $pfx) {
-        Try {
-            Remove-Item $p -Force
-        }
-        Catch {
-            Write-Warning "Unable to remove $($p)"
-        }
-        
-        ForEach ($p in $p12) {
-            Try {
-                Remove-Item $p -Force
-            }
-            Catch {
-                Write-Warning "Unable to remove $($p)"
-            }
-        }
+ForEach ($p in $p12) {
+    Try {
+        Remove-Item $p -Force
+    }
+    Catch {
+        Write-Warning "Unable to remove '$($p)'"
     }
 }
